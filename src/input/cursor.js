679366ee6f38028d9ac590ab9239beb3f36da2f6
Ricksters2000 <1012rvelez@gmail.com>
2021-12-23 16:40:03 -0500

    added comments for better readability

--- src/input/cursor.js
+++ src/input/cursor.js
@@ -15,12 +15,14 @@ const handleCursorTrackingToggle = (e, lblCursor) => {
 const handleMouseHover = (e) => {
     if(!trackCursor) return
 
+    //if the cursor was on a different tile in the grid then set that previous tile back to its original state  
     if(dest && dest.x < rows && dest.y < cols) grid.setTile(dest.y, dest.x, prevType || FLOOR)
 
     const [y, x] = grid.getIndexFromCoords(e.offsetX, e.offsetY)
     dest = new Point(x, y)
     prevType = grid.setTile(dest.y, dest.x, DEST)
 
+    //don't restart everything if cursor is still on the same tile
     if(aStar.dest.equals(dest)) return
 
     aStar.dest = dest
